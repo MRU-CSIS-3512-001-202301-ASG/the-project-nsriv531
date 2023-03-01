882c5ea2c2b442e9ae9528e551276741b94d33a3
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2023-03-01 21:16:11 +0000

    changes

---
+++ htdocs/database/query-helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+
+function image_grabber($db_helper) {
+
+    $query = <<<QUERY
+
+    SELECT imagedetails.ImageID, imagedetails.Path, cities.AsciiName, countries.CountryName, imagedetails.Latitude, 
+    imagedetails.Longitude, imagerating.rating FROM imagedetails 
+    INNER JOIN imagerating ON imagedetails.ImageID  = imagerating.ImageID  
+    INNER JOIN cities ON imagedetails.CityCode = cities.CityCode 
+    INNER JOIN countries ON cities.CountryCodeISO = countries.ISO 
+    WHERE imagerating.UserID = 23 
+    ORDER BY rating DESC
+
+    QUERY;
+
+    return $db_helper->run($query)->fetchAll(PDO::FETCH_COLUMN);
+}
+
+
+?>
