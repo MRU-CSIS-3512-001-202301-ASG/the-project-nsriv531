a78d6f4cdfc1b0316fe7ecebc9a7b85982168009
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2023-03-12 07:55:46 +0000

    changes to admin

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
 
         if ($username == "admin" && $password == "password") {
