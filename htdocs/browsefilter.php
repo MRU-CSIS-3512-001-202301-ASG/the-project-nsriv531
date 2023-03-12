ee712924779df01e9eb4061a427eea1add07df61
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2023-03-12 07:27:14 +0000

    no magic numbers being added?

--- htdocs/browsefilter.php
+++ htdocs/browsefilter.php
@@ -31,6 +31,7 @@ if (!isset($_SESSION["loginkey"])) {
         $orderby = "City";
         $orderASCDESC = "ASC";
         setcookie("citycookie", "ASC", time() + (86400 * 30), "/");
+
     } else if (isset($_COOKIE["citycookie"])  && isset($_GET["citybutton"])) {
 
         $orderby = "City";
@@ -51,6 +52,7 @@ if (!isset($_SESSION["loginkey"])) {
         $orderby = "Rating";
         $orderASCDESC = "ASC";
         setcookie("ratingcookie", "ASC", time() + (86400 * 30), "/");
+
     } else if (isset($_COOKIE["ratingcookie"])  && isset($_GET["ratingbutton"])) {
 
         $orderby = "Rating";
@@ -58,7 +60,9 @@ if (!isset($_SESSION["loginkey"])) {
         if ($_COOKIE["ratingcookie"] == "ASC") {
 
             setcookie("ratingcookie", "DESC", time() + (86400 * 30), "/");
+
             $orderASCDESC = "DESC";
+
         } else if ($_COOKIE["ratingcookie"] == "DESC") {
 
             setcookie("ratingcookie", "ASC", time() + (86400 * 30), "/");
@@ -84,7 +88,7 @@ if (!isset($_SESSION["loginkey"])) {
         $andCondition = $_GET['searchtext'];
     }
 
-    for ($i = 1; $i <= 148; $i++) {
+    for ($i = 1; $i <= sizeof($queryResult = image_grabber($db_helper, $orderby, $orderASCDESC, $andWHERE, $andCondition)); $i++) {
         $paramname = 'rateselector' . $i;
         if (!empty($_GET[$paramname]) && isset($_GET["submitbutton"])) {
 
@@ -103,7 +107,6 @@ if (!isset($_SESSION["loginkey"])) {
     if (empty($queryResult)) {
         $emptyQuery = "Filter produced no results.";
     }
-
 
     require 'browsefilter.view.php';
 }
