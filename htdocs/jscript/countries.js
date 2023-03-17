84d34adb4dcae707c602c32b9ff8613e83906aec
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2023-03-17 22:02:33 +0000

    changes for the div

--- htdocs/jscript/countries.js
+++ htdocs/jscript/countries.js
@@ -1,8 +1,9 @@
-const countries = [
+const countries = ['Canada', 'United States', 'Jamaica'];
 
-    'Canada',
-    'United States',
-    'Jamaica'
+var countryDiv = document.getElementById("countrydiv");
 
-]
-
+for (var i = 0; i < countries.length; i++) {
+    var countryItem = document.createElement("p");
+    countryItem.textContent = countries[i];
+    countryDiv.appendChild(countryItem);
+}
