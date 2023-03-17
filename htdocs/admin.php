ea9b7b2e373de1f04928cba9c68593d2680cf7b3
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2023-03-17 02:02:14 +0000

    changes made

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -15,10 +15,6 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
 
         $username = $_POST["username"];
         $password = $_POST["password"];
-
-        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-         echo $hashed_password;
-
         if ($username == "admin" && $password == "password") {
 
             $_SESSION["loginkey"] = true;
