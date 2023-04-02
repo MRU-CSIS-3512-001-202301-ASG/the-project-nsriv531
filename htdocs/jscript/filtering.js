fd25a04704867ba91bd8f0cfd8fbdc5c40866e11
nsriv531 <77025122+nsriv531@users.noreply.github.com>
2023-04-02 09:11:30 +0000

    added proper filtering

--- htdocs/jscript/filtering.js
+++ htdocs/jscript/filtering.js
@@ -10,27 +10,28 @@ citySearchBox.addEventListener('input', filterCities);
 function filterCountries() {
   const filter = countrySearchBox.value.toUpperCase();
   const pTags = countryDiv.querySelectorAll('p');
-  
-  for (let i = 0; i < pTags.length; i++) {
-    const countryName = pTags[i].textContent.toUpperCase();
-    if (countryName.includes(filter)) {
-      pTags[i].style.display = 'block';
+
+  pTags.forEach(pTag => {
+    const countryName = pTag.textContent.toUpperCase();
+    if (countryName.startsWith(filter)) {
+      pTag.style.display = 'block';
     } else {
-      pTags[i].style.display = 'none';
+      pTag.style.display = 'none';
     }
-  }
+  });
 }
 
 function filterCities() {
   const filter = citySearchBox.value.toUpperCase();
   const pTags = cityDiv.querySelectorAll('p');
-  
-  for (let i = 0; i < pTags.length; i++) {
-    const cityName = pTags[i].textContent.toUpperCase();
-    if (cityName.includes(filter)) {
-      pTags[i].style.display = 'block';
+
+  pTags.forEach(pTag => {
+    const cityName = pTag.textContent.toUpperCase();
+    if (cityName.startsWith(filter)) {
+      pTag.style.display = 'block';
     } else {
-      pTags[i].style.display = 'none';
+      pTag.style.display = 'none';
     }
-  }
+  });
 }
+
